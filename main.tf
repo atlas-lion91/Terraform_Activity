8efd8e84a404da9f82d387f87039eace3b0eb00a
Khalil Elkharbibi <140761974+atlas-lion91@users.noreply.github.com>
2023-10-11 10:54:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "aws" {
   secret_key = ""
   region = "us-east-1"
 }
-
+# This is so we can let terraform know which VPC we would like to work with
 data "aws_vpc" "dep4-vpc" {
   id = "vpc-0f20e6dacc9facc75"
 }
 
 # create instance
-resource "aws_instance" "Deployment4_EC2" {
+resource "aws_instance" "Deployment4_EC2_2" {
   # input what OS ID you will use
   ami = "ami-053b0d53c279acc90"
   # input the instance type according to business needs
@@ -27,7 +27,7 @@ resource "aws_instance" "Deployment4_EC2" {
   user_data = "${file("deploy.sh")}"
 
   tags = {
-    "Name": "Deployment4_EC2"
+    "Name": "Deployment4_EC2_2"
   }
 }
 # request an output of the IP address of the newly created EC2
